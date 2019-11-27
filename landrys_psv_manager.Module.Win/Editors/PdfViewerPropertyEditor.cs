f9b19c6626b549a3651502c7c6c5ea3762088f88
David Landry <dwlandry@gmail.com>
2019-11-27 04:14:55 -0600

    Added PdfViewerPropertyEditor : WinPropertyEditor
    Added to landrys_psv_manager.Module.Win.Editors
    This strategy was implemented from https://www.devexpress.com/Support/Center/Question/Details/T252349/pdfviewer-in-xaf

---
+++ landrys_psv_manager.Module.Win/Editors/PdfViewerPropertyEditor.cs
@@ -0,0 +1,71 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="D:\Users\dlandry\source\repos\landrys-psv-manager\landrys_psv_manager.Module.Win\Editors\PdfViewerPropertyEditor.cs" company="David W. Landry III">
+//     Author: _**David Landry**_
+//     *Copyright (c) David W. Landry III. All rights reserved.*
+// </copyright>
+//-----------------------------------------------------------------------
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Win.Editors;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.XtraPdfViewer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace landrys_psv_manager.Module.Win.Editors
+{
+    [PropertyEditor(typeof(FileData), false)]
+    public class PdfViewerPropertyEditor : WinPropertyEditor
+    {
+        protected PdfViewerPropertyEditor(Type objectType, IModelMemberViewItem model) : base(objectType, model) { }
+
+        public new PdfViewer Control => ((PdfViewer)base.Control);
+
+        protected override void Dispose(bool disposing)
+        {
+            try
+            {
+                if (disposing && Control != null)
+                {
+                    Control.DocumentChanged -= PdfViewerOnDocumentChanged;
+                }
+            }
+            finally
+            {
+                base.Dispose(disposing);
+            }
+        }
+        protected override void ReadValueCore()
+        {
+            var fileData = PropertyValue as IFileData;
+            if (fileData != null && fileData.FileName.ToLower().Contains(".pdf"))
+            {
+                using (var stream = new MemoryStream())
+                {
+                    fileData.SaveToStream(stream);
+                    Control.LoadDocument(stream);
+                }
+            }
+            else
+            {
+                Control.CloseDocument();
+            }
+        }
+        protected override object CreateControlCore()
+        {
+            var pdfViewer = new PdfViewer() { DetachStreamAfterLoadComplete = true };
+            pdfViewer.DocumentChanged += PdfViewerOnDocumentChanged;
+            return pdfViewer;
+        }
+        private void PdfViewerOnDocumentChanged(object sender, PdfDocumentChangedEventArgs e)
+        {
+            //OnControlValueChanged();
+            //Control.Refresh();
+        }
+    }
+}
